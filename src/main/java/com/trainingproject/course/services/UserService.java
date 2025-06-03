5da6af62d77b8685e41cdc636cec5cbc67209594
Erick Jesus <erickontiveros017@gmail.com>
2025-06-03 14:59:53 -0300

    Exception handling - update

--- src/main/java/com/trainingproject/course/services/UserService.java
+++ src/main/java/com/trainingproject/course/services/UserService.java
@@ -4,11 +4,14 @@ import com.trainingproject.course.entities.User;
 import com.trainingproject.course.repositories.UserRepository;
 import com.trainingproject.course.services.exceptions.DataBaseException;
 import com.trainingproject.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Optional;
 
@@ -33,7 +36,7 @@ public class UserService {
 
     public void delete(Long id) {
         try {repository.deleteById(id);}
-        catch (EmptyResultDataAccessException e) {
+        catch (ResourceNotFoundException e) {
             throw new ResourceNotFoundException(id);
         } catch (DataIntegrityViolationException e) {
             throw new DataBaseException(e.getMessage());
@@ -41,9 +44,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+       try {
+           User entity = repository.getReferenceById(id);
+           updateData(entity, obj);
+           return repository.save(entity);
+       }
+       catch (EntityNotFoundException e) {
+           throw new ResourceNotFoundException(id);
+       }
     }
 
     private void updateData(User entity, User obj) {
